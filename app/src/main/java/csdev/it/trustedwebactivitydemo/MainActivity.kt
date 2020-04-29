a2c067c7dac0beb32c5981fc1d953d4dcc78c777
Christian Schaefer <cs85@me.com>
2020-04-29 18:00:24 +0200

    adds twa dependency

--- app/src/main/java/csdev/it/trustedwebactivitydemo/MainActivity.kt
+++ app/src/main/java/csdev/it/trustedwebactivitydemo/MainActivity.kt
@@ -1,19 +1,30 @@
 package csdev.it.trustedwebactivitydemo
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.androidbrowserhelper.trusted.TwaLauncher
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var twaLauncher: TwaLauncher
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        twaLauncher = TwaLauncher(this)
+
         startTwaButton.setOnClickListener { launchTwa() }
     }
 
     private fun launchTwa() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val uri = Uri.parse(CONTENT_URL)
+        twaLauncher.launch(uri)
+    }
+
+    companion object{
+        private const val CONTENT_URL = "https://schaeferchristian.github.io/trustedwebactivitydemo/"
     }
 }

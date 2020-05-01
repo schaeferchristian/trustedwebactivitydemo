6c7970913bc7c6ef385ee08b1d421139377692b5
Christian Schaefer <cs85@me.com>
2020-05-01 12:33:07 +0200

    handles parameters from URL / TWA

--- app/src/main/java/csdev/it/trustedwebactivitydemo/LaunchFromWebActivity.kt
+++ app/src/main/java/csdev/it/trustedwebactivitydemo/LaunchFromWebActivity.kt
@@ -3,6 +3,7 @@ package csdev.it.trustedwebactivitydemo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_launch_from_web.*
 
 class LaunchFromWebActivity : AppCompatActivity() {
@@ -10,6 +11,10 @@ class LaunchFromWebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_from_web)
+        supportActionBar.apply {
+            this?.setHomeButtonEnabled(true)
+            this?.setDisplayHomeAsUpEnabled(true)
+        }
 
         handleIntent()
     }
@@ -20,11 +25,21 @@ class LaunchFromWebActivity : AppCompatActivity() {
         handleIntent()
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun handleIntent() {
-//        intent.action
       intent.data?.let {
-          val msg = it.lastPathSegment
-          messageTextView.text = msg
+          messageTextView.text = it.getQueryParameter("message")
+          sourceTextView.text = it.getQueryParameter("source")
+          param3TextView.text = it.getQueryParameter("param3")
       }
     }
 }
